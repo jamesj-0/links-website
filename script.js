873e584380d447f83818473d0af13787f0d7b2d1
James-Hardy <jamesjhardy95@gmail.com>
2020-04-04 17:19:36 +0100

    added form

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const linksSection = document.querySelector('.links')
+const contactButton = document.getElementById('contact__button')
+const contactUs = document.getElementsByClassName('contact')[0]
+const container = document.getElementsByClassName('contact__container')[0]
+const closeButton = document.getElementById('close')
+const allInput = document.querySelectorAll('input')
+const submitButton = document.querySelector('.form__submit')
+const submitResult = document.querySelector('.form__submit-message')
+
 let linksArr = [
   {
     link: 'url.com',
@@ -13,12 +21,22 @@ let linksArr = [
   {
     link: 'url.com',
     title: 'Mixes',
-    index: '4',
+    index: '2',
   },
   {
     link: 'url.com',
-    title: 'Mixes3',
-    index: '3',
+    title: 'Mixes',
+    index: '2',
+  },
+  {
+    link: 'url.com',
+    title: 'Mixes',
+    index: '2',
+  },
+  {
+    link: 'url.com',
+    title: 'Mixes',
+    index: '2',
   },
 ]
 
@@ -28,6 +46,69 @@ linksArr.map(item => {
   newLinkItem.href = item.link
   newLinkItem.style.webkitOrder = item.index
   newLinkItem.className = `links__button item-${item.index}`
-  newLinkItem.style.padding = `1.3rem`
+  newLinkItem.style.padding = `0.9rem`
+  newLinkItem.style.margin = calcMargin(linksArr)
   linksSection.appendChild(newLinkItem)
 })
+
+function calcMargin(arr) {
+  let margin = 0
+  switch (arr.length) {
+    case 1:
+      margin = '1.9rem'
+      break
+    case 2:
+      margin = '1rem'
+      break
+    case 3:
+      margin = '0.9rem'
+      break
+    case 4:
+      margin = '0.7rem'
+      break
+    case 5:
+      margin = '0.7rem'
+      break
+    case 6:
+      margin = '0.4rem'
+      break
+  }
+  return margin
+}
+
+contactButton.onclick = () => {
+  formAppear()
+}
+
+submitButton.onclick = e => {
+  for (let i = 0; i < allInput.length; i++) {
+    if (allInput[i].value !== '') {
+      e.preventDefault()
+      submitResult.style.display = 'flex'
+      setTimeout(() => {
+        submitResult.style.display = 'none'
+        formDisappear()
+      }, 2000)
+    }
+  }
+}
+
+closeButton.onclick = () => {
+  formDisappear()
+}
+
+document.onclick = event => {
+  if (event.target == container) {
+    formDisappear()
+  }
+}
+
+function formAppear() {
+  contactUs.style.display = 'flex'
+  contactButton.style.display = 'none'
+}
+
+function formDisappear() {
+  contactUs.style.display = 'none'
+  contactButton.style.display = 'flex'
+}
