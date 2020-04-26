dc2646a9a1872a930355fd02f060babe1316e904
James-Hardy <jamesjhardy95@gmail.com>
2020-04-26 21:12:16 +0100

    add & remove Iframe on button pressed

--- linklisteners.js
+++ linklisteners.js
@@ -1,20 +1,32 @@
+let testBool = false
+
 function linkListeners() {
   const links = Array.from(document.querySelectorAll('.links__button'))
   links.map((item) => {
     item.addEventListener('click', (e) => {
       e.preventDefault()
-      console.log(item.id)
-      widget(item)
+      testBool = !testBool
+      testBool ? addWidget(item) : removeWidget(item)
     })
   })
 }
 
-function widget(item) {
-  var para = document.createElement('P') // Create a <p> node
-  var t = document.createTextNode(`${item.id}`) // Create a text node
-  para.appendChild(t)
-  console.log(item)
-  document.getElementById(`${item.id}`).appendChild(para) // Append <p> to <div> with id="myDIV"
+function removeWidget(item) {
+  document.querySelectorAll(`#${item.id}`)[2].remove()
+}
+
+function addWidget(item) {
+  let widget = document.createRange().createContextualFragment(
+    `<iframe id=${item.id}
+      src="https://open.spotify.com/embed/playlist/1xn1QZMdGIZkjPBkIBPZ8y"
+      width="400"
+      height="80"
+      frameborder="0"
+      allowtransparency="true"
+      allow="encrypted-media"
+    ></iframe>`,
+  )
+  document.getElementById(`${item.id}`).appendChild(widget)
 }
 
 export { linkListeners }
